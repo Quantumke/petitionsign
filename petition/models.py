63c28c4f4b60f2457faf44d7db36f1240b195bd4
Benson Mburu <nguruben@gmail.com>
2016-02-08 00:13:28 +0300

    models to display first page

--- petition/models.py
+++ petition/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class petition(models.Model):
+	firstname = models.CharField(max_length=100, unique=False)
+	lastname = models.CharField(max_length=100, unique=False)
+	date = models.DateField(max_length=100, unique =False)
+
+	def __unicode__(self):
+		return '%s' %self.firstname
