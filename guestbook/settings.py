93686a9d2c8f45fbceecea29372fcf411c3ea3f6
Benson Mburu <nguruben@gmail.com>
2016-02-08 00:48:17 +0300

    idk

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -100,6 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS =(os.path.join(os.path.dirname(BASE_DIR),"static"),)
 STATICFILES_ROOT =os.path.join(os.path.dirname(BASE_DIR),"static")
